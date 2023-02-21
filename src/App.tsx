6a4995c76162e5a3c6af5ff9ffeaecc168fb6893
Oleksandr Balitskiy <popingalov@gmail.com>
2023-02-21 19:55:29 +0300

    start
    start dependencies

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+const styleTest: ITest = {
+  color: 'red',
+};
+
+function App() {
+  return (
+    <div>
+      <h1 style={styleTest}>Hi my big World</h1>
+    </div>
+  );
+}
+
+export default App;
